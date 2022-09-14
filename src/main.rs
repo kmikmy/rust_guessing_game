33c1318555be8affc78f178ee4f55a68d143b69c
Komei Kamiya <komei.kamiya@gmail.com>
2022-09-15 00:00:18 +0900

    refactor: create domain

--- src/main.rs
+++ src/main.rs
@@ -2,29 +2,47 @@ use std::io;
 use std::cmp::Ordering;
 use rand::Rng;
 
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Self {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {value}.");
+        }
+
+        Guess { value }
+    }
+
+    pub fn value(&self) -> i32 {
+        self.value
+    }
+}
+
 fn main() {
     println!("Guess the number!");
     let secret_number = rand::thread_rng().gen_range(1..101);
 
     loop {
-        println!("Please input your guess.");
+        println!("Please input your guess (between 1 and 100).");
         let mut guess=String::new();
     
         io::stdin()
             .read_line(&mut guess)
             .expect("Failed to read line");
 
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
+        let guess = match guess.trim().parse() {
+            Ok(num) => Guess::new(num),
             Err(_) => {
                 println!("Please type a number!");
                 continue;
             }
         };
 
-        println!("Your guessed: {}", guess);
+        println!("Your guessed: {}", guess.value());
 
-        match guess.cmp(&secret_number) {
+        match guess.value().cmp(&secret_number) {
             Ordering::Less => println!("Too small"),
             Ordering::Greater => println!("Too big"),
             Ordering::Equal => {
